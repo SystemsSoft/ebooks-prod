d1a88c6c4b6a07c33dd33dd14a787a67dcf8c2ef
SystemsSoft <devmob60@gmail.com>
2026-03-11 14:27:47 -0300

    Add files via upload

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -38,6 +38,6 @@ _flutter.buildConfig = {"engineRevision":"c29809135135e262a912cf583b2c90deb9ded6
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2375382500"
+    serviceWorkerVersion: "1088630111"
   }
 });
